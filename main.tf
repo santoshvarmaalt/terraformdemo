a758f24212baf3741fe7f51c8eab0e7455ebb379
santoshvarmaalt <121477094+santoshvarmaalt@users.noreply.github.com>
2022-12-27 14:53:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,32 +12,32 @@ provider "aws" {
   # Configuration options
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+# resource "aws_vpc" "my_vpc" {
+#   cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
+# resource "aws_subnet" "my_subnet" {
+#   vpc_id            = aws_vpc.my_vpc.id
+#   cidr_block        = "172.16.10.0/24"
+#   availability_zone = "us-east-1a"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
+#   tags = {
+#     Name = "tf-example"
+#   }
+# }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+# resource "aws_network_interface" "foo" {
+#   subnet_id   = aws_subnet.my_subnet.id
+#   private_ips = ["172.16.10.100"]
 
-  tags = {
-    Name = "primary_network_interface"
-  }
-}
+#   tags = {
+#     Name = "primary_network_interface"
+#   }
+# }
 
 resource "aws_instance" "foo" {
   ami           = "ami-0b5eea76982371e91" # us-west-2
